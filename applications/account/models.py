bfeec5ca713417fb0e09a963e9eeb7dac63214b2
g1ftmoon <orozakunov2222@gmail.com>
2022-03-21 20:38:03 +0600

    Add favorite

--- applications/account/models.py
+++ applications/account/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from applications.product.models import Product
+
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -43,6 +45,7 @@ class Profile(models.Model):
     photo = models.ImageField(upload_to='users.photo', blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
+    favorite = models.ManyToManyField(Product, related_name='favorite', blank='True')
 
     def __str__(self):
         return self.user.email
--- applications/product/urls.py
+++ applications/product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
-from applications.product.views import ProductListView, ProductDetailView
+from applications.product.views import ProductListView, ProductDetailView, FavoriteView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('<int:pk>/', ProductDetailView.as_view()),
+    path('<int:pk>/favorite/', FavoriteView.as_view()),
 ]
